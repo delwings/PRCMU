52b2167976419e2ec0a299a47c70ac8c757c14cc
delwings <274301808+delwings@users.noreply.github.com>
2026-04-08 22:36:48 -0500

    correction directory rcmu

--- src/main/java/edu/unimagdalena/RCMU/RcmuApplication.java
+++ src/main/java/edu/unimagdalena/RCMU/RcmuApplication.java
@@ -1,4 +1,4 @@
-package edu.unimagdalena.PRCMU;
+package edu.unimagdalena.RCMU;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
