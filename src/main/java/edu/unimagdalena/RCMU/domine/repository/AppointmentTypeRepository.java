d574ea0f8c9eb5a3829fc6879dd626c325933435
delwings <274301808+delwings@users.noreply.github.com>
2026-04-10 14:58:10 -0500

    feat repo

--- src/main/java/edu/unimagdalena/RCMU/domine/repository/AppointmentTypeRepository.java
+++ src/main/java/edu/unimagdalena/RCMU/domine/repository/AppointmentTypeRepository.java
@@ -4,6 +4,10 @@ import edu.unimagdalena.RCMU.domine.entity.AppointmentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppointmentTypeRepository extends JpaRepository<AppointmentType, Long> {
+    // Para evitar duplicados y buscar por nombre exacto
+    Optional<AppointmentType> findByName(String name);
 }
