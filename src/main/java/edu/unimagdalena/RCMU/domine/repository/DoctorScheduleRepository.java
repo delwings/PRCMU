6edebfb9af944df79cc65c605e5f13fadc94493a
delwings <274301808+delwings@users.noreply.github.com>
2026-04-10 14:58:33 -0500

    feat repo

--- src/main/java/edu/unimagdalena/RCMU/domine/repository/DoctorScheduleRepository.java
+++ src/main/java/edu/unimagdalena/RCMU/domine/repository/DoctorScheduleRepository.java
@@ -1,10 +1,18 @@
 package edu.unimagdalena.RCMU.domine.repository;
 
+import edu.unimagdalena.RCMU.domine.entity.DoctorSchedule;
+import edu.unimagdalena.RCMU.domine.enums.DayOfWeek;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Para los horarios de los doctores
+import java.util.List;
+
+
 @Repository
-public interface DoctorScheduleRepository extends JpaRepository<edu.unimagdalena.RCMU.domine.entity.DoctorSchedule, Long> {
-    java.util.List<edu.unimagdalena.RCMU.domine.entity.DoctorSchedule> findByDoctorId(Long doctorId);
+public interface DoctorScheduleRepository extends JpaRepository<DoctorSchedule, Long> {
+    // Busca los horarios asignados a un doctor específico
+    List<DoctorSchedule> findByDoctorId(Long doctorId);
+
+    // Filtra horarios por doctor y día de la semana usando el Enum DayOfWeek
+    List<DoctorSchedule> findByDoctorIdAndDayOfWeek(Long doctorId, DayOfWeek dayOfWeek);
 }
