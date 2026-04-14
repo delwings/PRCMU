b9a7efaa273e22ddb20befa0618ae3d9dce72466
delwings <274301808+delwings@users.noreply.github.com>
2026-04-14 15:44:12 -0500

    add speciality service impl

---
+++ src/main/java/edu/unimagdalena/RCMU/service/impl/SpecialityServiceImpl.java
@@ -0,0 +1,33 @@
+package edu.unimagdalena.RCMU.services.impl;
+
+import edu.unimagdalena.RCMU.api.dto.SpecialityDtos.*;
+import edu.unimagdalena.RCMU.domine.repositories.SpecialityRepository;
+import edu.unimagdalena.RCMU.services.SpecialtyService;
+import edu.unimagdalena.RCMU.services.mapper.SpecialityMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SpecialityServiceImpl implements SpecialityService {
+
+    private final SpecialityRepository repo;
+
+    @Override
+    public SpecialityResponse create(CreateSpecialityRequest req) {
+        var entity = SpecialityMapper.toEntity(req);
+        return SpecialityMapper.toResponse(repo.save(entity));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<SpecialityResponse> findAll() {
+        return repo.findAll().stream()
+                .map(SpecialityMapper::toResponse)
+                .toList();
+    }
+}
