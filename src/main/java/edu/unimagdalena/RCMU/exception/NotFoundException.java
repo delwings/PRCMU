7d19c5322a4fccd4f14a774a9215c90c9dec36c1
delwings <274301808+delwings@users.noreply.github.com>
2026-04-20 22:46:45 -0500

    delete exception directory and move class to api-

--- src/main/java/edu/unimagdalena/RCMU/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.unimagdalena.RCMU.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
