89d5825361eadea015dc98e72e8843d6ae314ab8
delwings <274301808+delwings@users.noreply.github.com>
2026-04-21 15:36:52 -0500

    new feat: doctor service

--- src/main/java/edu/unimagdalena/RCMU/service/DoctorService.java
+++ src/main/java/edu/unimagdalena/RCMU/service/DoctorService.java
@@ -1,11 +1,13 @@
 package edu.unimagdalena.RCMU.service;
 
 import edu.unimagdalena.RCMU.api.dto.DoctorDtos.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface DoctorService {
     DoctorResponse create(CreateDoctorRequest req);
     DoctorResponse update(Long id, UpdateDoctorRequest req);
     DoctorResponse getById(Long id);
-    List<DoctorResponse> findAll();
+    Page<DoctorResponse> findAll(Pageable pageable);
 }
