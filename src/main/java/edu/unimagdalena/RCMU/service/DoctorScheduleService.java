b7343aa42606299ea06a1992c7c573b866bbbe89
delwings <274301808+delwings@users.noreply.github.com>
2026-04-22 16:05:54 -0500

    a bit fix

--- src/main/java/edu/unimagdalena/RCMU/service/DoctorScheduleService.java
+++ src/main/java/edu/unimagdalena/RCMU/service/DoctorScheduleService.java
@@ -4,6 +4,7 @@ import edu.unimagdalena.RCMU.api.dto.DoctorScheduleDtos.*;
 import java.util.List;
 
 public interface DoctorScheduleService {
-    DoctorScheduleResponse create(CreateDoctorScheduleRequest req);
+    // Firma actualizada para coincidir con el Controller
+    DoctorScheduleResponse create(Long doctorId, CreateDoctorScheduleRequest req);
     List<DoctorScheduleResponse> findByDoctorId(Long doctorId);
 }
